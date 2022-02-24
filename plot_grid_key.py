2e7cbf51e3b99671dbc3f9a3262f7cbbf61aa0bc
Pankayaraj <pankayaraj1995@gmail.com>
2022-02-24 10:52:58 +0530

    random

--- plot_grid_key.py
+++ plot_grid_key.py
@@ -18,6 +18,10 @@ R_safe = []
 C_safe = []
 HRL_R_safe = []
 HRL_C_safe = []
+HRL_R_safe_2 = []
+HRL_C_safe_2 = []
+HRL_R_safe_3 = []
+HRL_C_safe_3 = []
 R_k = []
 C_k = []
 
@@ -29,8 +33,14 @@ for i in range(no_experiments):
     C_safe.append(torch.load("results/grid/safe_sarsa_key/c" + str(i+1))[0:l])
     R_k.append(torch.load("results/grid/hrl_sarsa_key/r" + str(i + 1))[:l])
     C_k.append(torch.load("results/grid/hrl_sarsa_key/c" + str(i + 1))[0:l])
-    HRL_R_safe.append(torch.load("results/grid/safe_lower_hrl_sarsa_key/r" + str(i + 1))[:l])
-    HRL_C_safe.append(torch.load("results/grid/safe_lower_hrl_sarsa_key/c" + str(i + 1))[0:l])
+    HRL_R_safe.append(torch.load("results/grid/safe_lower_hrl_sarsa_key/c_10/r" + str(i + 1))[:l])
+    HRL_C_safe.append(torch.load("results/grid/safe_lower_hrl_sarsa_key/c_10/c" + str(i + 1))[0:l])
+
+    if i < 3:
+        HRL_R_safe_2.append(torch.load("results/grid/safe_lower_hrl_sarsa_key/c_20/r" + str(i + 1))[:l])
+        HRL_C_safe_2.append(torch.load("results/grid/safe_lower_hrl_sarsa_key/c_20/c" + str(i + 1))[0:l])
+        HRL_R_safe_3.append(torch.load("results/grid/safe_lower_hrl_sarsa_key/c_30/r" + str(i + 1))[:l])
+        HRL_C_safe_3.append(torch.load("results/grid/safe_lower_hrl_sarsa_key/c_30/c" + str(i + 1))[0:l])
 
 R_avg = []
 C_avg = []
@@ -38,6 +48,10 @@ R_safe_avg = []
 C_safe_avg = []
 HRL_R_safe_avg = []
 HRL_C_safe_avg = []
+HRL_R_safe_avg_2 = []
+HRL_C_safe_avg_2 = []
+HRL_R_safe_avg_3 = []
+HRL_C_safe_avg_3 = []
 R_k_avg = []
 C_k_avg = []
 
@@ -48,6 +62,10 @@ for j in range(len(R)):
     c_safe_avg = []
     hrl_r_safe_avg = []
     hrl_c_safe_avg = []
+    hrl_r_safe_avg_2 = []
+    hrl_c_safe_avg_2 = []
+    hrl_r_safe_avg_3 = []
+    hrl_c_safe_avg_3 = []
     r_k_avg = []
     c_k_avg = []
     for i in range(10,len(R_safe[0])):
@@ -57,6 +75,12 @@ for j in range(len(R)):
         c_safe_avg.append(np.mean(C_safe[j][i-10:i]))
         hrl_r_safe_avg.append(np.mean(HRL_R_safe[j][i - 10:i]))
         hrl_c_safe_avg.append(np.mean(HRL_C_safe[j][i - 10:i]))
+
+        if j < 3:
+            hrl_r_safe_avg_2.append(np.mean(HRL_R_safe_2[j][i - 10:i]))
+            hrl_c_safe_avg_2.append(np.mean(HRL_C_safe_2[j][i - 10:i]))
+            hrl_r_safe_avg_3.append(np.mean(HRL_R_safe_3[j][i - 10:i]))
+            hrl_c_safe_avg_3.append(np.mean(HRL_C_safe_3[j][i - 10:i]))
         r_k_avg.append(np.mean(R_k[j][i - 10:i]))
         c_k_avg.append(np.mean(C_k[j][i - 10:i]))
 
@@ -66,17 +90,30 @@ for j in range(len(R)):
     C_safe_avg.append(c_safe_avg)
     HRL_R_safe_avg.append(hrl_r_safe_avg)
     HRL_C_safe_avg.append(hrl_c_safe_avg)
+    if j < 3:
+        HRL_R_safe_avg_2.append(hrl_r_safe_avg_2)
+        HRL_C_safe_avg_2.append(hrl_c_safe_avg_2)
+        HRL_R_safe_avg_3.append(hrl_r_safe_avg_3)
+        HRL_C_safe_avg_3.append(hrl_c_safe_avg_3)
     R_k_avg.append(r_k_avg)
     C_k_avg.append(c_k_avg)
 
+
+print(len(HRL_R_safe_avg_2), len(HRL_R_safe_avg_2[0]))
 R_avg = np.array(R_avg)
 C_avg = np.array(C_avg)
 R_safe_avg = np.array(R_safe_avg)
 C_safe_avg = np.array(C_safe_avg)
 HRL_R_safe_avg = np.array(HRL_R_safe_avg)
 HRL_C_safe_avg = np.array(HRL_C_safe_avg)
+HRL_R_safe_avg_2 = np.array(HRL_R_safe_avg_2)
+HRL_C_safe_avg_2 = np.array(HRL_C_safe_avg_2)
+HRL_R_safe_avg_3 = np.array(HRL_R_safe_avg_3)
+HRL_C_safe_avg_3 = np.array(HRL_C_safe_avg_3)
 R_k_avg = np.array(R_k_avg)
 C_k_avg = np.array(C_k_avg)
+
+
 
 R_mean = np.mean(R_avg, axis=0)
 C_mean = np.mean(C_avg, axis=0)
@@ -84,9 +121,12 @@ R_safe_mean = np.mean(R_safe_avg, axis=0)
 C_safe_mean = np.mean(C_safe_avg, axis=0)
 HRL_R_safe_mean = np.mean(HRL_R_safe_avg, axis=0)
 HRL_C_safe_mean = np.mean(HRL_C_safe_avg, axis=0)
+HRL_R_safe_mean_2 = np.mean(HRL_R_safe_avg_2, axis=0)
+HRL_C_safe_mean_2 = np.mean(HRL_C_safe_avg_2, axis=0)
+HRL_R_safe_mean_3 = np.mean(HRL_R_safe_avg_3, axis=0)
+HRL_C_safe_mean_3 = np.mean(HRL_C_safe_avg_3, axis=0)
 R_k_mean = np.mean(R_k_avg, axis=0)
 C_k_mean = np.mean(C_k_avg, axis=0)
-
 
 R_std = np.std(R_avg, axis=0)
 C_std = np.std(C_avg, axis=0)
@@ -94,12 +134,16 @@ R_safe_std = np.std(R_safe_avg, axis=0)
 C_safe_std = np.std(C_safe_avg, axis=0)
 HRL_R_safe_std = np.std(HRL_R_safe_avg, axis=0)
 HRL_C_safe_std = np.std(HRL_C_safe_avg, axis=0)
+HRL_R_safe_std_2 = np.std(HRL_R_safe_avg_2, axis=0)
+HRL_C_safe_std_2 = np.std(HRL_C_safe_avg_2, axis=0)
+HRL_R_safe_std_3 = np.std(HRL_R_safe_avg_3, axis=0)
+HRL_C_safe_std_3 = np.std(HRL_C_safe_avg_3, axis=0)
 R_k_std = np.std(R_k_avg, axis=0)
 C_k_std = np.std(C_k_avg, axis=0)
 
 
 x = [i for i in range(len(R_safe_mean))]
-legend = ["sarsa", "bvf-safe-sarsa-eps", "hrl-sarsa", "hrl-safe-low-level-sarsa-manual-cost-alloc" ]
+legend = ["sarsa", "bvf-safe-sarsa-eps", "hrl-sarsa", "hrl-safe-l-c-alloc-10", "hrl-safe-low-l-c-alloc-20", "hrl-safe-l-c-alloc-30" ]
 
 fig, ax = plt.subplots(1, 1, figsize=(20, 10))
 plt.tick_params(axis='both', which='major', labelsize=30)
@@ -116,7 +160,13 @@ plt.fill_between(x, R_k_mean + R_k_std, R_k_mean - R_k_std, alpha = 0.1)
 plt.plot(x, HRL_R_safe_mean, label=legend[3], linewidth=5)
 plt.fill_between(x, HRL_R_safe_mean + HRL_R_safe_std, HRL_R_safe_mean - HRL_R_safe_std, alpha = 0.1)
 
-plt.legend( prop={'size':40})
+plt.plot(x, HRL_R_safe_mean_2, label=legend[4], linewidth=5)
+plt.fill_between(x, HRL_R_safe_mean_2 + HRL_R_safe_std_2, HRL_R_safe_mean_2 - HRL_R_safe_std_2, alpha = 0.1)
+
+plt.plot(x, HRL_R_safe_mean_3, label=legend[5], linewidth=5)
+plt.fill_between(x, HRL_R_safe_mean_3 + HRL_R_safe_std_3, HRL_R_safe_mean_3 - HRL_R_safe_std_3, alpha = 0.1)
+
+plt.legend( prop={'size':40}, loc='upper left')
 plt.xlabel('No of episodes X1000', size=40)
 plt.ylabel("Reward", size=40)
 plt.title("Reward comparision for the Grid World Environment with a key", size=40)
@@ -140,9 +190,16 @@ plt.fill_between(x, C_k_mean + C_k_std, C_k_mean - C_k_std, alpha = 0.3)
 plt.plot(HRL_C_safe_mean, label=legend[3], linewidth=3)
 plt.fill_between(x, HRL_C_safe_mean + HRL_C_safe_std, HRL_C_safe_mean - HRL_C_safe_std, alpha = 0.3)
 
+plt.plot(HRL_C_safe_mean_2, label=legend[4], linewidth=3)
+plt.fill_between(x, HRL_C_safe_mean_2 + HRL_C_safe_std_2, HRL_C_safe_mean_2 - HRL_C_safe_std_2, alpha = 0.3)
+
+plt.plot(HRL_C_safe_mean_3, label=legend[5], linewidth=3)
+plt.fill_between(x, HRL_C_safe_mean_3 + HRL_C_safe_std_3, HRL_C_safe_mean_3 - HRL_C_safe_std_3, alpha = 0.3)
+
+
 
 plt.axhline(y=20, color='r', linestyle='--')
-plt.legend(prop={'size':40})
+plt.legend(prop={'size':40}, loc='upper left')
 plt.xlabel('No of episodes X1000', size=40)
 plt.ylabel("Constraints", size=40)
 plt.title("Constraint comparision for the Grid World Environment with a key", size=40)
